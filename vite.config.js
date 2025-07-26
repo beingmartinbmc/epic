a43d526c4f000196fdcca55998ec6bad69a798cb
ankit-d-sharma <ankit.s@games24x7.com>
2025-07-26 20:26:08 +0530

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/epic/', // Replace with your repository name
+  base: '/epic/',
   build: {
     outDir: 'dist',
-    emptyOutDir: true, // Clean the output directory before building
-    rollupOptions: {
-      input: 'index.html' // Now it's in the root
-    }
-  },
-  // Clear cache on build
-  server: {
-    fs: {
-      strict: false
-    }
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 }); 
